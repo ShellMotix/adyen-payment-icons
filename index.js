f9e5a91a6344e39cfe8d6e05cb76f7c2c1facbbf
Szymon Wierzchowski <szymonwierzchowski@Szymons-Mac-mini.local>
2019-03-27 13:57:49 +0000

    npm init

---
+++ index.js
@@ -0,0 +1,67 @@
+import paypal from './paypal.png'
+import paypalEcs from './paypal_ecs.png'
+import klarna from './klarna.png'
+import afterpayDefault from './afterpay_default.png'
+import sepadirectdebit from './sepadirectdebit.png'
+import ideal from './ideal.png'
+import cup from './cup.png'
+import cartebancaire from './cartebancaire.png'
+import visa from './visa.png'
+import mc from './mc.png'
+import uatp from './uatp.png'
+import amex from './amex.png'
+import maestro from './maestro.png'
+import maestrouk from './maestrouk.png'
+import diners from './diners.png'
+import discover from './discover.png'
+import visadankort from './visadankort.png'
+import jcb from './jcb.png'
+import laser from './laser.png'
+import solo from './solo.png'
+import bcmc from './bcmc.png'
+import bijcard from './bijcard.png'
+import dankort from './dankort.png'
+import hipercard from './hipercard.png'
+import elo from './elo.png'
+import visaalphabankbonus from './visaalphabankbonus.png'
+import mcalphabankbonus from './mcalphabankbonus.png'
+import karenmillen from './karenmillen.png'
+import oasis from './oasis.png'
+import warehouse from './warehouse.png'
+import unionpay from './unionpay.png'
+import mir from './mir.png'
+
+export default {
+  paypal,
+  paypalEcs,
+  klarna,
+  afterpayDefault,
+  sepadirectdebit,
+  ideal,
+  cup,
+  cartebancaire,
+  visa,
+  mc,
+  uatp,
+  amex,
+  maestro,
+  maestrouk,
+  diners,
+  discover,
+  visadankort,
+  jcb,
+  laser,
+  solo,
+  bcmc,
+  bijcard,
+  dankort,
+  hipercard,
+  elo,
+  visaalphabankbonus,
+  mcalphabankbonus,
+  karenmillen,
+  oasis,
+  warehouse,
+  unionpay,
+  mir
+}
